03698743a7336b7bc35d363d391f30c473ceb501
shank0020 <shashank.cse20@gmail.com>
2020-08-16 17:21:46 +0530

    Created ‘Header’ Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React, { Fragment } from 'react';
+import Header from "./Header"
 
 function App() {
+  let x = 10, y = 20;
   return (
+    
     <div>
-        Phone Directory
-        <div className="header">
-          Phone Directory
-        </div>
+        <Header/>
         <button>Add</button>
         <div>
           <span>Name</span><br />
           <span>Phone</span>
         </div>
+        <input type="text" placeholder="value" defaultValue={x+y}></input>
       </div>
   );
 }
